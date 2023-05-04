34d5d5af3155d2064a5a914c291e6440f1802860
BryanMic <bryan.michel.via@gmail.com>
2023-05-04 14:12:32 +0200

    index js
    affichage postman

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ connect.connect(function(err){
 })
 
 app.get("/garage", function(request, response){
-    connect.query("SELECT * from garage;" , function(err, result){
+    connect.query("SELECT * from voiture;" , function(err, result){
         if(err) throw err;
         console.log(result);
         response.status(200).json(result);
@@ -45,7 +45,7 @@ app.get("/", function (request,response){
 })
 
 app.post('/garage', (request, response) => {
-    const querys = "INSERT INTO garage (marques, models, kilometres) values ('"+request.body.marques+"','"+request.body.models+"','"+request.body.kilometres+"');"
+    const querys = "INSERT INTO voiture (marques, models, kilometres) values ('"+request.body.marques+"','"+request.body.models+"','"+request.body.kilometres+"');"
     console.log(querys);
     console.query(querys, function (err,result){
         if(err) throw err;
@@ -59,7 +59,7 @@ app.post('/garage', (request, response) => {
 })
 
 app.get("/garage/:id", function(request,response){
-    connect.query("SELECT * from garage WHERE id="+request.params.id+";", function(err, result){
+    connect.query("SELECT * from voiture WHERE id="+request.params.id+";", function(err, result){
 
         if(err) throw err;
         console.log(result);
@@ -75,7 +75,7 @@ app.get("/garage/:id", function(request,response){
 
 app.delete('/garage/:id', (request, response) => {
     const id = parseInt(request/params/id);
-    const query = "DELETE FROM GARAGE WHERE id="+id+ ";"
+    const query = "DELETE FROM VOITURE WHERE id="+id+ ";"
     connect.query(query, function(err, result){
         if (err) throw err;
         console.log(result);
